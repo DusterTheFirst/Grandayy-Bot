3ac3f4cbadbf78cd3b99049ad5e605bb793432a0
Zachary Kohnen <DusterTheFirst@users.noreply.github.com>
2017-05-13 21:35:22 -0400

    yuutube...

--- Robbie Botten/SocialMedia/Youtube.cs
+++ Robbie Botten/SocialMedia/Youtube.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using Discord.WebSocket;
+using RobbieBotten.Config;
+using RobbieBotten.Util;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
-namespace RobbieBotten.SocialMedia
-{
-    class Youtube
-    {
+namespace RobbieBotten.SocialMedia {
+    public class Youtube {
+        private ConfigFile config;
+        private SocketTextChannel channel;
+        private string UUID;
+        private string name;
+
+        Timer timer;
+
+        public Youtube(ConfigFile config, SocketTextChannel channel, string UUID, string name) {
+            this.config = config;
+            this.channel = channel;
+            this.UUID = UUID;
+            this.name = name;
+
+            Logger.Log($"[Youtube Handler:{name}] Started Youtube checker");
+
+            timer = new Timer(Update, null, 0, config.SocialTick);
+        }
+
+        public void Update(object StateInfo) {
+
+        }
     }
 }
